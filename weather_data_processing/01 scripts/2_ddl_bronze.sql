964fc80703db66b2ecc9832cc4be726d2254e3a5
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2026-07-15 23:00:43 +0200

    Add files via upload

---
+++ weather_data_processing/01 scripts/2_ddl_bronze.sql
@@ -0,0 +1,35 @@
+/*
+============================================================================
+DDL Bronze Script: Create Raw Data Table
+============================================================================
+Script Purpose:
+  This script creates table dropping existing table
+  if it already exists.
+Run this script to redefine the Bronze DDL structure.
+============================================================================
+*/
+
+DROP TABLE IF EXISTS bronze.weather;
+CREATE TABLE bronze.weather (
+    station VARCHAR(11),
+    observation_date DATE,
+    metric VARCHAR(4),
+    value INTEGER,
+    measurement_flag CHAR(1),
+    quality_flag CHAR(1),
+    source_flag CHAR(1),
+    observation_time VARCHAR(4)
+);
+
+DROP TABLE IF EXISTS bronze.stations;
+CREATE TABLE bronze.stations (
+    station VARCHAR(11),
+    latitude NUMERIC(8,4),
+    longitude NUMERIC(9,4),
+    elevation NUMERIC(6,1),
+    state VARCHAR(50),
+    station_name VARCHAR(100),
+    gsn_flag VARCHAR(3),
+    hcn_flag VARCHAR(3),
+    wmo_id NUMERIC(6,1)
+);
