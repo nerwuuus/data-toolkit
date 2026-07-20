b5512ac7d92033529f6d048a85863fafde2cc2d4
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2026-07-20 21:25:13 +0200

    Delete weather_data_processing/01 scripts directory

--- weather_data_processing/01 scripts/1_create_schemas.sql
+++
@@ -1,9 +0,0 @@
-/*
-===============================================================
-Create Schemas
-===============================================================
-*/
-
-CREATE SCHEMA IF NOT EXISTS bronze;
-CREATE SCHEMA IF NOT EXISTS silver;
-CREATE SCHEMA IF NOT EXISTS gold;
