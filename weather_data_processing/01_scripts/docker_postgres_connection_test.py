ccd29e393883dada1e4939eea15416fdeb498ba6
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2026-09-09 22:37:27 +0200

    script update

--- weather_data_processing/01_scripts/docker_postgres_connection_test.py
+++ weather_data_processing/01_scripts/docker_postgres_connection_test.py
@@ -1,3 +1,16 @@
 import psycopg2
 
+conn = psycopg2.connect(
+    host="host.docker.internal",
+    port=5433,
+    dbname="analytics_db",
+    user="postgres",
+    password="admin"
+)
 
+cur = conn.cursor()
+print("Connected.")
+
+cur.execute(
+    "SELECT * FROM test;"
+)
