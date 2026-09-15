4ebc211c47c96b488a9a8250492b8178a76b5429
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2026-09-15 19:17:20 +0200

    Add Docker Compose database write test

--- weather_data_processing/01_scripts/docker_postgres_connection_test.py
+++ weather_data_processing/01_scripts/docker_postgres_connection_test.py
@@ -10,15 +10,33 @@ conn = psycopg2.connect(
     password="admin"
 )
 
-# Placeholder
-
 cur = conn.cursor()
 print("Connected.")
 
 # Execute the SQL query. Doesn't print anything in the console.
-cur.execute(
-    "SELECT current_database();"
-)
+cur.execute("DROP TABLE IF EXISTS docker_test;")
+
+cur.execute("""
+    CREATE TABLE docker_test (
+        id INTEGER,
+        name TEXT
+    );
+""")
+
+cur.execute("""
+    INSERT INTO docker_test (id, name)
+    VALUES
+        (1, 'Python'),
+        (2, 'PostgreSQL');
+""")
+
+conn.commit()
+
+cur.execute(""" \
+    "SELECT * " \
+    "FROM docker_test " \
+    "ORDER BY id;"
+""")
 
 # Fetch all rows from the query result and
 # display the result in the terminal
