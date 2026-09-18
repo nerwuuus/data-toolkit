e8ead0639e86145495e83d6ba27dfbb21ce944de
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2026-09-18 21:40:22 +0200

    Data types updated: NUMERIC to (10,2), station VARCHAR to (55)

--- weather_data_processing/01_scripts/5_ddl_silver.sql
+++ weather_data_processing/01_scripts/5_ddl_silver.sql
@@ -11,17 +11,17 @@ Run this script to redefine the DDL structure of 'silver' Tables.
 
 DROP TABLE IF EXISTS silver.weather CASCADE;
 CREATE TABLE silver.weather (
-    station VARCHAR(11),
+    station VARCHAR(55),
     observation_date DATE,
     metric VARCHAR(4),
-    value NUMERIC(5,1),
+    value NUMERIC(10,2),
     insert_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS silver.stations CASCADE;
 CREATE TABLE silver.stations (
-    station VARCHAR(11),
-    elevation NUMERIC(6,1),
+    station VARCHAR(55),
+    elevation NUMERIC(10,2),
     station_name VARCHAR(100),
     insert_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
